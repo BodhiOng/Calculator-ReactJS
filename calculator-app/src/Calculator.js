d21081390460a3fac2eeefd46a54201bec0e78d4
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-07-22 11:44:20 +0700

    Made some new buttons

--- calculator-app/src/Calculator.js
+++ calculator-app/src/Calculator.js
@@ -60,22 +60,26 @@ const Calculator = () => {
           </div>
           
           <div className="buttons">
+            <button onClick={handleClear} className='clear-button'>C</button>
+            <button>( )</button>
+            <button>%</button>
+            <button onClick={() => handleClick('+')} className='operator-button'>+</button>
             <button onClick={() => handleClick('1')}>1</button>
             <button onClick={() => handleClick('2')}>2</button>
             <button onClick={() => handleClick('3')}>3</button>
-            <button onClick={() => handleClick('+')} className='operator-button'>+</button>
+            <button onClick={() => handleClick('-')} className='operator-button'>-</button>
             <button onClick={() => handleClick('4')}>4</button>
             <button onClick={() => handleClick('5')}>5</button>
             <button onClick={() => handleClick('6')}>6</button>
-            <button onClick={() => handleClick('-')} className='operator-button'>-</button>
+            <button onClick={() => handleClick('*')} className='operator-button'>×</button>
             <button onClick={() => handleClick('7')}>7</button>
             <button onClick={() => handleClick('8')}>8</button>
             <button onClick={() => handleClick('9')}>9</button>
-            <button onClick={() => handleClick('*')} className='operator-button'>*</button>
+            <button onClick={() => handleClick('/')} className='operator-button'>÷</button>
+            <button>+/-</button>
             <button onClick={() => handleClick('0')}>0</button>
-            <button onClick={handleClear} className='clear-button'>C</button>
+            <button>.</button>
             <button onClick={handleCalculate} className='equal-button'>=</button>
-            <button onClick={() => handleClick('/')} className='operator-button'>/</button>
           </div>
         </div>
       </div>
