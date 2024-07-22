2a6ec3eff7a603a5c1e9aec6b4933be2cbd496d3
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-07-22 21:57:27 +0700

    Design completely responsive now

--- calculator-app/src/Header.js
+++ calculator-app/src/Header.js
@@ -5,14 +5,12 @@ import './Header.css';
     return (
       <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
         <div className="container-fluid position-relative">
-          <div className="row w-100">
+          <div className="row w-100 align-items-center">
             <div className="col text-start"></div>
-
-              <div className="col text-center">
-                <a className="navbar-brand custom-navbar-brand">Calculator</a>
-              </div>
-
-              <div className="col text-end"></div>
+            <div className="col text-center">
+              <a className="navbar-brand custom-navbar-brand">Calculator</a>
+            </div>
+            <div className="col text-end"></div>
           </div>
           <a href="https://github.com/BodhiOng" className="text-light custom-developer-link">About the Developer</a>
         </div>
