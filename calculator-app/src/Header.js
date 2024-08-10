21cd02724398b0483827179cfb373caf531a7923
Bodhidharma Ong <bodhidharma202@gmail.com>
2024-08-10 21:44:45 +0700

    Fixed responsiveness of calculator and removed redundant divs on header

--- calculator-app/src/Header.js
+++ calculator-app/src/Header.js
@@ -6,15 +6,8 @@ import './Header.css';
       <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
         <div className="container-fluid position-relative">
           <div className="row w-100 align-items-center">
-            <div className="col text-start">
-              {/* Left side of the navbar, currently empty */}
-            </div>
             <div className="col text-center">
-              {/* Centered brand name */}
               <a className="navbar-brand custom-navbar-brand">Calculator</a>
-            </div>
-            <div className="col text-end">
-              {/* Right side of the navbar, currently empty */}
             </div>
           </div>
           {/* Link to the developer's Github profile */}
--- calculator-app/src/index.js
+++ calculator-app/src/index.js
@@ -1,9 +1,8 @@
-import React from 'react'; // Import React library
-import { createRoot } from 'react-dom/client'; // Import createRoot from React DOM client for rendering
-import reportWebVitals from './reportWebVitals'; // Import reportWebVitals for performance measurement
-import Calculator from './Calculator'; // Import the Calculator component
-import './Calculator.css'; // Import CSS styles for the Calculator components
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS for styling
+import React from 'react'; 
+import { createRoot } from 'react-dom/client';
+import Calculator from './Calculator'; 
+import './Calculator.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 // Get the root DOM element where the React app will be rendered
 const container = document.getElementById('root');
@@ -15,8 +14,3 @@ root.render(
     <Calculator />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
